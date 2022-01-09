865f188b1d14be4918a7e011ad867bf65648e334
Stanislav <77130336+standbit@users.noreply.github.com>
2022-01-09 17:54:19 +0300

    Show who is in storage and how long

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils.timezone import localtime
 
 
 class Passcard(models.Model):
@@ -25,3 +27,39 @@ class Visit(models.Model):
             entered=self.entered_at,
             leaved= 'leaved at ' + str(self.leaved_at) if self.leaved_at else 'not leaved'
         )
+
+
+def get_unfinished_visits():
+    all_visits = Visit.objects.all()
+    unfinished_visits = []
+    for visit in all_visits:
+        if not visit.leaved_at:
+            unfinished_visits.append(visit)
+    return unfinished_visits
+
+
+def get_duration(visit):
+    now = localtime().replace(microsecond=0)
+    entered_time = localtime(visit.entered_at)
+    delta = now - entered_time
+    return delta
+
+
+def format_duration(duration):
+    seconds = int(duration.total_seconds())
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    secs = (seconds % 3600) % 60
+    return f'{hours}:{minutes}:{secs}'
+
+
+def get_visit_description(visits):
+    visits_description = []
+    for lost_visit in visits:
+        descript = {
+            'who_entered': lost_visit.passcard,
+            'entered_at': localtime(lost_visit.entered_at),
+            'duration': format_duration(get_duration(lost_visit)),
+        }
+        visits_description.append(descript)
+    return visits_description
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,4 +1,4 @@
-from datacenter.models import Passcard
+from datacenter.models import Passcard, get_unfinished_visits, get_visit_description
 from datacenter.models import Visit
 from django.shortcuts import render
 
@@ -6,13 +6,14 @@ from django.shortcuts import render
 def storage_information_view(request):
     # Программируем здесь
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+    non_closed_visits = get_visit_description(get_unfinished_visits())
+    # non_closed_visits = [
+    #     {
+    #         'who_entered': 'Richard Shaw',
+    #         'entered_at': '11-04-2018 25:34',
+    #         'duration': '25:03',
+    #     }
+    # ]
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
