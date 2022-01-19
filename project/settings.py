636f7d8e14e2c45f003be5a45c7178e03758d8b5
Stanislav <77130336+standbit@users.noreply.github.com>
2022-01-19 23:24:43 +0300

    Hide secret info from settings

--- project/settings.py
+++ project/settings.py
@@ -1,19 +1,22 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
+        'HOST': os.getenv("DB_HOST"),
         'PORT': '5434',
         'NAME': 'checkpoint',
         'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'PASSWORD': os.getenv("DB_PASSWORD"),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.getenv("DB_SECRET_KEY")
 
 DEBUG = True
 
